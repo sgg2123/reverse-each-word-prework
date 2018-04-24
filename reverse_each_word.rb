84debc7d5f96d9eaf27bd37379a3ff4d98af4067
Sydney Garay <sgg2123@columbia.edu>
2018-04-24 13:24:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   arr = string.split(" ")
-  result = []
-  arr.each do |item|
-    result.push(item.reverse)
+  result = arr.collect do |item|
+    item.reverse
   end
   result.join(" ")
 end
